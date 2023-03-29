233ab9c8fb8c1dae834f110b27a511d0b8ee3f36
Tahir Hameed <33459018+TahirHameed74@users.noreply.github.com>
2023-03-29 19:53:24 +0500

    Add files via upload

---
+++ blocksdk/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlockSdk;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Webhook bas = BlockSDK.createWebHOOK("B1zZARyW1d2FdqWxPUpB79izHmtAc2Az693WF9DD");
+            var res = bas.listPrice();
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ blocksdk/TestLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    class Program { 
+        static async Task Main(string[] args)
+        {
+            
+            var bas = new Base("aaa");
+            bas.request("GET", "", new String[] { "" });
+
+
+        }
+    }
+}
